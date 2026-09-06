7a3b41017a734bd7df6983e227033cea74710b2c
gracimarch <gracianamarch1@gmail.com>
2026-09-06 20:59:02 -0300

    tp3

---
+++ tp3/abm/src/App.js
@@ -0,0 +1,269 @@
+import React from "react";
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import {
+  Table,
+  Button,
+  Container,
+  Modal,
+  ModalHeader,
+  ModalBody,
+  FormGroup,
+  ModalFooter,
+} from "reactstrap";
+
+// Datos iniciales de prueba
+const data = [
+  { id: 1, personaje: "Naruto", anime: "Naruto" },
+  { id: 2, personaje: "Goku", anime: "Dragon Ball" },
+  { id: 3, personaje: "Kenshin Himura", anime: "Rurouni Kenshin" },
+  { id: 4, personaje: "Monkey D. Luffy", anime: "One Piece" },
+  { id: 5, personaje: "Edward Elric", anime: "Fullmetal Alchemist" },
+  { id: 6, personaje: "Seto Kaiba", anime: "Yu-Gi-Oh!" },
+];
+
+class App extends React.Component {
+  state = {
+    data: data,
+    modalActualizar: false,
+    modalInsertar: false,
+    form: {
+      id: "",
+      personaje: "",
+      anime: "",
+    },
+  };
+
+  // Actualiza el estado del formulario al escribir en los inputs
+  handleChange = (e) => {
+    this.setState({
+      form: {
+        ...this.state.form,
+        [e.target.name]: e.target.value,
+      },
+    });
+  };
+
+  // Métodos para controlar el modal de Insertar
+  mostrarModalInsertar = () => {
+    this.setState({ modalInsertar: true });
+  };
+
+  ocultarModalInsertar = () => {
+    this.setState({ modalInsertar: false });
+  };
+
+  // Métodos para controlar el modal de Actualizar / Editar
+  mostrarModalActualizar = (dato) => {
+    this.setState({
+      form: dato,
+      modalActualizar: true,
+    });
+  };
+
+  ocultarModalActualizar = () => {
+    this.setState({ modalActualizar: false });
+  };
+
+  // Operación: Insertar un nuevo elemento
+  insertar = () => {
+    var valorNuevo = { ...this.state.form };
+    valorNuevo.id = this.state.data.length + 1;
+    var lista = this.state.data;
+    lista.push(valorNuevo);
+    this.setState({ data: lista, modalInsertar: false });
+  };
+
+  // Operación: Editar un elemento existente
+  editar = (dato) => {
+    var contador = 0;
+    var arreglo = this.state.data;
+    arreglo.map((registro) => {
+      if (dato.id === registro.id) {
+        arreglo[contador].personaje = dato.personaje;
+        arreglo[contador].anime = dato.anime;
+      }
+      contador++;
+    });
+    this.setState({ data: arreglo, modalActualizar: false });
+  };
+
+  // Operación: Eliminar un elemento de la lista
+  eliminar = (dato) => {
+    var opcion = window.confirm(
+      "¿Estás seguro que deseas eliminar el elemento " + dato.id + "?"
+    );
+    if (opcion === true) {
+      var contador = 0;
+      var arreglo = this.state.data;
+      arreglo.map((registro) => {
+        if (dato.id === registro.id) {
+          arreglo.splice(contador, 1);
+        }
+        contador++;
+      });
+      this.setState({ data: arreglo, modalActualizar: false });
+    }
+  };
+
+  render() {
+    return (
+      <>
+        <Container>
+          <br />
+          <Button color="success" onClick={() => this.mostrarModalInsertar()}>
+            Crear Nuevo Personaje
+          </Button>
+          <br />
+          <br />
+
+          {/* Tabla principal (Consulta) */}
+          <Table>
+            <thead>
+              <tr>
+                <th>ID</th>
+                <th>Personaje</th>
+                <th>Anime</th>
+                <th>Acciones</th>
+              </tr>
+            </thead>
+            <tbody>
+              {this.state.data.map((dato) => (
+                <tr key={dato.id}>
+                  <td>{dato.id}</td>
+                  <td>{dato.personaje}</td>
+                  <td>{dato.anime}</td>
+                  <td>
+                    <Button
+                      color="primary"
+                      onClick={() => this.mostrarModalActualizar(dato)}
+                    >
+                      Editar
+                    </Button>{" "}
+                    {"  "}
+                    <Button color="danger" onClick={() => this.eliminar(dato)}>
+                      Eliminar
+                    </Button>
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </Table>
+        </Container>
+
+        {/* Modal Insertar */}
+        <Modal isOpen={this.state.modalInsertar}>
+          <ModalHeader>
+            <div>
+              <h3>Insertar Personaje</h3>
+            </div>
+          </ModalHeader>
+
+          <ModalBody>
+            <FormGroup>
+              <label>Id:</label>
+              <input
+                className="form-control"
+                readOnly
+                type="text"
+                value={this.state.data.length + 1}
+              />
+            </FormGroup>
+
+            <FormGroup>
+              <label>Personaje:</label>
+              <input
+                className="form-control"
+                name="personaje"
+                type="text"
+                onChange={this.handleChange}
+              />
+            </FormGroup>
+
+            <FormGroup>
+              <label>Anime:</label>
+              <input
+                className="form-control"
+                name="anime"
+                type="text"
+                onChange={this.handleChange}
+              />
+            </FormGroup>
+          </ModalBody>
+
+          <ModalFooter>
+            <Button color="primary" onClick={() => this.insertar()}>
+              Insertar
+            </Button>
+            <Button
+              className="btn btn-danger"
+              onClick={() => this.ocultarModalInsertar()}
+            >
+              Cancelar
+            </Button>
+          </ModalFooter>
+        </Modal>
+
+        {/* Modal Editar */}
+        <Modal isOpen={this.state.modalActualizar}>
+          <ModalHeader>
+            <div>
+              <h3>Editar Registro</h3>
+            </div>
+          </ModalHeader>
+
+          <ModalBody>
+            <FormGroup>
+              <label>Id:</label>
+              <input
+                className="form-control"
+                readOnly
+                type="text"
+                value={this.state.form.id}
+              />
+            </FormGroup>
+
+            <FormGroup>
+              <label>Personaje:</label>
+              <input
+                className="form-control"
+                name="personaje"
+                type="text"
+                onChange={this.handleChange}
+                value={this.state.form.personaje}
+              />
+            </FormGroup>
+
+            <FormGroup>
+              <label>Anime:</label>
+              <input
+                className="form-control"
+                name="anime"
+                type="text"
+                onChange={this.handleChange}
+                value={this.state.form.anime}
+              />
+            </FormGroup>
+          </ModalBody>
+
+          <ModalFooter>
+            <Button
+              color="primary"
+              onClick={() => this.editar(this.state.form)}
+            >
+              Editar
+            </Button>
+            <Button
+              color="danger"
+              onClick={() => this.ocultarModalActualizar()}
+            >
+              Cancelar
+            </Button>
+          </ModalFooter>
+        </Modal>
+      </>
+    );
+  }
+}
+
+export default App;
---
+++ tp3/login-signup/src/components/LoginSignup/LoginSignup.jsx
@@ -0,0 +1,61 @@
+import React, { useState } from "react";
+import "./LoginSignup.css";
+
+import user_icon from "../../assets/person.png";
+import email_icon from "../../assets/email.png";
+import password_icon from "../../assets/password.png";
+
+const LoginSignup = () => {
+  const [action, setAction] = useState("Sign Up");
+
+  return (
+    <div className="container">
+      <div className="header">
+        <div className="text">{action}</div>
+        <div className="underline"></div>
+      </div>
+
+      <div className="inputs">
+        {action === "Login" ? null : (
+          <div className="input">
+            <img src={user_icon} alt="User Icon" />
+            <input type="text" placeholder="Name" />
+          </div>
+        )}
+
+        <div className="input">
+          <img src={email_icon} alt="Email Icon" />
+          <input type="email" placeholder="Email Id" />
+        </div>
+
+        <div className="input">
+          <img src={password_icon} alt="Password Icon" />
+          <input type="password" placeholder="Password" />
+        </div>
+      </div>
+
+      {action === "Sign Up" ? null : (
+        <div className="forgot-password">
+          Lost password? <span>Click Here!</span>
+        </div>
+      )}
+
+      <div className="submit-container">
+        <div
+          className={action === "Login" ? "submit gray" : "submit"}
+          onClick={() => setAction("Sign Up")}
+        >
+          Sign Up
+        </div>
+        <div
+          className={action === "Sign Up" ? "submit gray" : "submit"}
+          onClick={() => setAction("Login")}
+        >
+          Login
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default LoginSignup;
